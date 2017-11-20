d64640c491a8a2e09ebe62372f414850bee5a3b2
Arturas Molcanovas <a.molcanovas@gmail.com>
2017-11-20 00:10:57 +0000

    skip chevron tests @ ng4

--- src/GhContribCalendar/Chevron/Chevron.spec.ts
+++ src/GhContribCalendar/Chevron/Chevron.spec.ts
@@ -2,7 +2,16 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {By} from '@angular/platform-browser';
 import {Chevron} from './Chevron';
 
-describe('Chevron', () => {
+let method: typeof describe | typeof xdescribe;
+
+if (process.env.CI_NG_VERSION === '4') {
+  console.warn('TestBed doesn\'t have all the features in ng4 - skipping');
+  method = xdescribe;
+} else {
+  method = describe;
+}
+
+method('Chevron', () => {
   let fixture: ComponentFixture<Chevron>;
   let inst: Chevron;
 
